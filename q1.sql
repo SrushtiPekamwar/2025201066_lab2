49966ee4af09a3510a9064d2c1fd86cea3005e10
Srushti Pekamwar <100013415+SrushtiPekamwar@users.noreply.github.com>
2025-08-16 11:44:17 +0530

    Update q1.sql

--- q1.sql
+++ q1.sql
@@ -10,23 +10,16 @@ BEGIN
 	DECLARE cur CURSOR FOR SELECT SubscriberName FROM Subscribers;
     -- handler
     DECLARE CONTINUE HANDLER FOR NOT FOUND SET finishedSearching = TRUE;
-    
-    -- Creating temporary table so that the output from the cursor can be stored 
-	CREATE TEMPORARY TABLE IF NOT EXISTS tempStorage (Subscriber VARCHAR(100));
-    TRUNCATE tempStorage;
 
     OPEN cur;
 		theLoop: LOOP
 			FETCH cur INTO subscriberNameFetched;
 				IF 
-					finishedSearching THEN LEAVE theLoop; -- if nothing fetched then it will leave the loop
+				finishedSearching THEN LEAVE theLoop; -- if nothing fetched then it will leave the loop
 				END IF;
-			INSERT INTO tempStorage VALUES (subscriberNameFetched);
+			SELECT CONCAT (subscriberNameFetched) AS SubscriberName;
 		END LOOP;
     CLOSE cur;
-    
-    -- Display the result fetched from the cursor
-    SELECT * FROM tempStorage;
 END //
 
 DELIMITER ;
